67b7d57eab6dbc69a284cadd62415d447d6b5a32
samparker9 <mesamparker.1998@gmail.com>
2024-01-29 13:36:23 +0530

    add database

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
+mongoose.connect('mongodb://localhost:27017/RegisterLogin', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
